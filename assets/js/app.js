bbc35fd6e9ba543dfecc3b1e997492af3884152f
RanobirRoy <98152636+RanobirRoy@users.noreply.github.com>
2022-06-11 20:47:20 +0600

    3rd commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,7 @@ const navToggle = document.querySelector(".nav-toggle");
 navToggle.addEventListener("click", () => {
     navItems.classList.toggle("nav-active");
 });
+const preloader = document.getElementById("preLoader");
+    function load(){
+        preloader.style.display = "none";
+    }
